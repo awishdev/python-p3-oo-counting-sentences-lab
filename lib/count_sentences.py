3b59a455007d5bb9e1531f94e3211eefbae65e99
Arthur Ish <arthurishdev@gmail.com>
2025-02-15 23:36:55 -0800

    that last test was a trixy hobbit

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  
+  def __init__(self, value=''):
+    self._value = ''
+    self.value = value
+
+  def set_value(self, value):
+    if isinstance(value, str):
+      self._value = value
+    else:
+      print("The value must be a string.")
+
+  def get_value(self):
+    return self._value
+  
+  value = property(get_value, set_value)
+
+  def is_sentence(self):
+    return self._value.endswith('.')
+  
+  def is_question(self):
+    return self._value.endswith('?')
+  
+  def is_exclamation(self):
+    return self._value.endswith('!')
+
+  def count_sentences(self):
+    modded_string = self._value.replace('.', '|').replace('?', '|').replace('!', '|')
+    return len([sentence for sentence in modded_string.split('|') if sentence.strip()])
